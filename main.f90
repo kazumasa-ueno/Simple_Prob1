a28274eecf25748a66313a503f963a561f3b88ac
kazumasa-ueno <kazumasashangye9@gmail.com>
2022-10-18 10:53:24 +0900

    temporary final version of two grid

--- main.f90
+++ main.f90
@@ -7,7 +7,9 @@ program main
 	real(8), parameter :: hf = X / N !細かい格子間隔
 	real(8), parameter :: hc = X / Nc !粗い格子間隔
 	real(8), parameter :: e0 = 8.85d-12 !真空の誘電率
-	integer, parameter :: nu1 = 10, nu2 = 5 !smoothing のステップ数上限
+	real(8), parameter :: Conv = 1.d-6 !収束判定用
+	integer, parameter :: nu1 = 1, nu2 = 5000 !smoothing のステップ数上限
+	integer :: ios
 	
 	real(8) :: phi(N,N) !計算する電位
 	real(8) :: rho(N,N) !電荷密度
@@ -19,13 +21,16 @@ program main
 
 	call initialize(phi, rho, f, Lf, Lc, Ifc, Icf, N, e0, hf)
 
-	call smooth(phi, f, Lf, N, hf, nu1)
+	call smooth(phi, f, Lf, N, hf, Conv, nu1)
 
-	call CGC(phi, Lf, Lc, Ifc, Icf, N, Nc, f, hc)
+	call CGC(phi, Lf, Lc, Ifc, Icf, N, Nc, f, hc, Conv)
 
-	call smooth(phi, f, Lf, N, hf, nu2)
+	call smooth(phi, f, Lf, N, hf, Conv, nu2)
 
-	write(*,*) "end program"
+	open(unit=10, file="./output/output.txt", iostat=ios, status="replace", action="write")
+	if ( ios /= 0 ) stop "Error opening file ./output/output.txt"
+	
+	write(10,*) phi(:,:)
 
 	stop
 contains
@@ -49,7 +54,7 @@ contains
 			end do
 		end do
 
-		f(:,:) = - rho(:,:) / e0
+		f(:,:) = rho(:,:) / e0
 
 		Lf( :, :) = 0.d0
 		Lf( 0, 0) = 4.d0
@@ -85,34 +90,57 @@ contains
 
 	end subroutine initialize
 
-	subroutine smooth(phi, f, Lf, N, hf, nu)
+	subroutine smooth(phi, f, Lf, N, hf, Conv, nu)
 		implicit none
 
-		real(8), intent(in) :: f(:,:), Lf(-1:1,-1:1), hf
+		real(8), intent(in) :: f(:,:), Lf(-1:1,-1:1), hf, Conv
 		integer, intent(in) :: N, nu
 		real(8), intent(inout) :: phi(:,:)
 
-		real(8) :: z(N,N)
-		real(8), parameter :: omega = 1.d0 
 		integer :: i, j, nt
+		real(8) :: Max !最大値
+		real(8) :: MaxErr !最大のエラー
+		real(8) :: CurErr !現在のエラー
+		real(8) :: Prev !前のループの値
 
+		Max = 1.d-5
 		do nt = 1, nu
+			MaxErr = 0.d0
+			CurErr = 0.d0
 			do j = 2, N-1
 				do i = 2, N-1
 					if(mod(i+j,2) == 0) then
-						z(i,j) = (hf**2 * f(i,j) + phi(i-1,j) + phi(i+1,j) + phi(i,j-1) + phi(i,j+1)) / 4.d0
+						Prev = phi(i,j)
+						phi(i,j) = (hf**2 * f(i,j) + phi(i-1,j) + phi(i+1,j) + phi(i,j-1) + phi(i,j+1)) / 4.d0
+						if(Max < abs(phi(i,j))) then 
+							Max = phi(i,j) 
+						end if
+						CurErr = (abs(phi(i,j) - Prev)) / Max
+						if(MaxErr < CurErr) then 
+							MaxErr = CurErr 
+						end if
 					end if
 				end do
 			end do
-			phi(:,:) = phi(:,:) + omega * (z(:,:) - phi(:,:))
 			do j = 2, N-1
 				do i = 2, N-1
 					if(mod(i+j,2) /= 0) then
-						z(i,j) = (hf**2 * f(i,j) + phi(i-1,j) + phi(i+1,j) + phi(i,j-1) + phi(i,j+1)) / 4.d0
+						Prev = phi(i,j)
+						phi(i,j) = (hf**2 * f(i,j) + phi(i-1,j) + phi(i+1,j) + phi(i,j-1) + phi(i,j+1)) / 4.d0
+						if(Max < abs(phi(i,j))) then 
+							Max = phi(i,j) 
+						end if
+						CurErr = (abs(phi(i,j) - Prev)) / Max
+						if(MaxErr < CurErr) then 
+							MaxErr = CurErr 
+						end if
 					end if
 				end do
 			end do
-			phi(:,:) = phi(:,:) + omega * (z(:,:) - phi(:,:))
+			if(MaxErr < Conv) then 
+				write(*,*) nt, MaxErr, Conv
+				exit
+			end if
 		end do
 
 	end subroutine smooth
@@ -186,45 +214,69 @@ contains
 
 	end subroutine Interpolation
 
-	subroutine CGC(phi, Lf, Lc, Ifc, Icf, N, Nc, f, hc)
+	subroutine CGC(phi, Lf, Lc, Ifc, Icf, N, Nc, f, hc, Conv)
 		implicit none
 		
-		real(8), intent(in) :: Lf(-1:1,-1:1), Lc(-1:1,-1:1), Ifc(-1:1,-1:1), Icf(-1:1,-1:1), f(:,:), hc
+		real(8), intent(in) :: Lf(-1:1,-1:1), Lc(-1:1,-1:1), Ifc(-1:1,-1:1), Icf(-1:1,-1:1), f(:,:), hc, Conv
 		real(8), intent(inout) :: phi(:,:)
 		integer, intent(in) :: N, Nc
 
-		real(8) :: df(N,N), dc(Nc,Nc), vf(N,N), vc(Nc,Nc), z(Nc,Nc)
-		integer :: i, j, nt
+		real(8) :: df(N,N), dc(Nc,Nc), vf(N,N), vc(Nc,Nc)
+		integer :: i, j, nt, loop
+		real(8) :: Max !最大値
+		real(8) :: MaxErr !最大のエラー
+		real(8) :: CurErr !現在のエラー
+		real(8) :: Prev !前のループの値
 
 		call stencil(Lf, phi, N)
 		df(:,:) = f(:,:) - phi(:,:)
 
 		call Restriction(Ifc, df, dc, N, Nc)
 
-		z(:,:) = 0.d0
-
-		do nt = 1, 10
+		Max = 1.d-10
+		MaxErr = 1.d0
+		loop = 0
+		do while(MaxErr > Conv)
+			MaxErr = 0.d0
+			CurErr = 0.d0
 			do j = 2, Nc-1
 				do i = 2, Nc-1
 					if(mod(i+j,2) == 0) then
-						z(i,j) = (hc**2 * dc(i,j) + vc(i-1,j) + vc(i+1,j) + vc(i,j-1) + vc(i,j+1)) / 4.d0
+						Prev = vc(i,j)
+						vc(i,j) = (hc**2 * dc(i,j) + vc(i-1,j) + vc(i+1,j) + vc(i,j-1) + vc(i,j+1)) / 4.d0
+						if(Max < abs(vc(i,j))) then 
+							Max = vc(i,j) 
+						end if
+						CurErr = (abs(vc(i,j) - Prev)) / Max
+						if(MaxErr < CurErr) then 
+							MaxErr = CurErr 
+						end if
 					end if
 				end do
 			end do
-			vc(:,:) = z(:,:)
 			do j = 2, Nc-1
 				do i = 2, Nc-1
 					if(mod(i+j,2) /= 0) then
-						z(i,j) = (hc**2 * dc(i,j) + vc(i-1,j) + vc(i+1,j) + vc(i,j-1) + vc(i,j+1)) / 4.d0
+						Prev = vc(i,j)
+						vc(i,j) = (hc**2 * dc(i,j) + vc(i-1,j) + vc(i+1,j) + vc(i,j-1) + vc(i,j+1)) / 4.d0
+						if(Max < abs(vc(i,j))) then 
+							Max = vc(i,j) 
+						end if
+						CurErr = (abs(vc(i,j) - Prev)) / Max
+						if(MaxErr < CurErr) then 
+							MaxErr = CurErr 
+						end if
 					end if
 				end do
 			end do
-			vc(:,:) = z(:,:)
+			loop = loop + 1
 		end do
 
 		call Interpolation(Icf, vf, vc, N, Nc)
 
 		phi(:,:) = phi(:,:) + vf(:,:)
+
+		write(*,*) loop
 
 	end subroutine CGC
 
